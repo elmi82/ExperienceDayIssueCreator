41cd7a7c3469de50f1725736ce18074bb176ec21
Timo Mueller <mail@timomueller.eu>
2015-03-16 16:08:03 +0100

    controller: Refactor controller
    Pull out some methods and add a more robust error handling.

--- src/main/java/edic/controller/IssueController.java
+++ src/main/java/edic/controller/IssueController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -41,7 +42,50 @@ public class IssueController {
 	private GithubConfiguration githubConfiguration;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String greetingForm(Model model) throws InvalidConfigurationException {
+	public String issueForm(Model model) throws InvalidConfigurationException {
+		try {
+			model.addAttribute("issueList", getIssueList());
+		} catch (RestClientException e) {
+			log.error("There was an error retrieving a list of issues on GitHub");
+		}
+
+		model.addAttribute("issue", new Issue());
+		return "issues";
+	}
+
+	@RequestMapping(value = "/issue", method = RequestMethod.POST)
+	public String issueSubmit(@ModelAttribute Issue issue, Model model)
+			throws InvalidConfigurationException {
+		Issue resultingIssue = createIssue(issue);
+
+		model.addAttribute("issue", resultingIssue);
+		return "result";
+	}
+
+	private Issue createIssue(Issue issue) throws InvalidConfigurationException {
+		HttpHeaders headers = getAuthorizationHeaders();
+		HttpEntity<Issue> request = new HttpEntity<Issue>(issue, headers);
+
+		URL url = githubConfiguration.getEndpointURL("issues");
+
+		try {
+			ResponseEntity<Issue> response = restTemplate.exchange(url.toString(),
+													HttpMethod.POST, request, Issue.class);
+			Issue resultingIssue = response.getBody();
+			return resultingIssue;
+		} catch (RestClientException e) {
+			log.error("There was an error creating the issue on GitHub");
+			throw e;
+		}
+	}
+
+	private HttpHeaders getAuthorizationHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Token " + githubConfiguration.getToken());
+		return headers;
+	}
+
+	private Set<Issue> getIssueList() throws InvalidConfigurationException, RestClientException {
 		HttpHeaders headers = getAuthorizationHeaders();
 		HttpEntity<Issue> request = new HttpEntity<Issue>(headers);
 
@@ -50,31 +94,7 @@ public class IssueController {
 		ResponseEntity<Set<Issue>> response = restTemplate.exchange(url.toString(),
 												HttpMethod.GET, request, responseType);
 		Set<Issue> issueList = response.getBody();
-
-		model.addAttribute("issueList", issueList);
-		model.addAttribute("issue", new Issue());
-		return "issues";
-	}
-
-	@RequestMapping(value = "/issue", method = RequestMethod.POST)
-	public String greetingSubmit(@ModelAttribute Issue issue, Model model)
-			throws InvalidConfigurationException {
-		HttpHeaders headers = getAuthorizationHeaders();
-		HttpEntity<Issue> request = new HttpEntity<Issue>(issue, headers);
-
-		URL url = githubConfiguration.getEndpointURL("issues");
-		ResponseEntity<Issue> response = restTemplate.exchange(url.toString(),
-												HttpMethod.POST, request, Issue.class);
-		Issue resultingIssue = response.getBody();
-
-		model.addAttribute("issue", resultingIssue);
-		return "result";
-	}
-
-	private HttpHeaders getAuthorizationHeaders() {
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Token " + githubConfiguration.getToken());
-		return headers;
+		return issueList;
 	}
 
 	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, // 500
