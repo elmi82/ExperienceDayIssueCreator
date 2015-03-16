d5621e22fb3a0f27bbf631e69ee04242ee6c9ec8
Timo Mueller <mail@timomueller.eu>
2015-03-16 10:12:36 +0100

    Add empty Spring Boot application

---
+++ src/main/java/edic/ExperienceDayIssueCreatorApplication.java
@@ -0,0 +1,12 @@
+package edic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperienceDayIssueCreatorApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(ExperienceDayIssueCreatorApplication.class, args);
+    }
+}
