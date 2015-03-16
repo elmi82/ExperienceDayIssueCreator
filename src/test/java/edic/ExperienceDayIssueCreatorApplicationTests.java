dc1e0115b0a10eef25af7a92ae1bf041d4bf984b
Timo Mueller <mail@timomueller.eu>
2015-03-16 15:29:26 +0100

    tests: Add a couple of tests

--- src/test/java/edic/ExperienceDayIssueCreatorApplicationTests.java
+++ src/test/java/edic/ExperienceDayIssueCreatorApplicationTests.java
@@ -1,18 +1,36 @@
 package edic;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import edic.controller.IssueController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ExperienceDayIssueCreatorApplication.class)
 @WebAppConfiguration
 public class ExperienceDayIssueCreatorApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new IssueController()).build();
 	}
 
+	@Test
+	public void indexPageLoads() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/")
+				.accept(MediaType.APPLICATION_XHTML_XML_VALUE))
+				.andExpect(status().isOk());
+	}
 }
