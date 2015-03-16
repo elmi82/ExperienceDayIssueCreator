9ae8dcb1b9a407b83a2b258385d6ce96c4a6f08c
Timo Mueller <mail@timomueller.eu>
2015-03-16 14:23:58 +0100

    exception: Add exception for invalid app configuration

---
+++ src/main/java/edic/exception/InvalidConfigurationException.java
@@ -0,0 +1,9 @@
+package edic.exception;
+
+public class InvalidConfigurationException extends Exception {
+	private static final long serialVersionUID = 3711946391068559799L;
+
+	public InvalidConfigurationException(String message) {
+		super(message);
+	}
+}
